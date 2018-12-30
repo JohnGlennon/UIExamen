bcfc7335c5847f96a6340b7f39bcbb9b01b4bfa2
User <glenn.geysen@student.kdg.be>
2018-12-30 17:28:39 +0100

    voor de verplaatsing van settings

--- src/app/room/room.component.ts
+++ src/app/room/room.component.ts
@@ -34,26 +34,17 @@ export class RoomComponent implements OnInit {
   }
 
   setStyle(): Object {
-    // if (this.rooms[this.currentRoomIndex].type === 'Auditorium' || this.rooms[this.currentRoomIndex].type === 'Classroom'
-    //   || this.rooms[this.currentRoomIndex].type === 'Meetingroom') {
-    //   return {
-    //     'background-color': this.occupiedChecked ? this.rooms[this.currentRoomIndex].occupied ? 'red' : 'green' : 'white',
-    //     'width': this.listChecked ? '40rem' : this.rooms[this.currentRoomIndex].width * 10 + 'px',
-    //     'height': this.listChecked ? '100px' : this.rooms[this.currentRoomIndex].height * 10 + 'px'
-    //   };
-    // } else {
     return {
       'background-color': this.getColor(),
       'width': this.listChecked ? '40rem' : this.rooms[this.currentRoomIndex].width * 10 + 'px',
       'height': this.listChecked ? '100px' : this.rooms[this.currentRoomIndex].height * 10 + 'px'
     };
-    // }
   }
 
   private getColor(): any {
     const blue = 0;
-    const maxRed = 200;
-    const maxGreen = 200;
+    const maxRed = 255;
+    const maxGreen = 255;
 
     if (this.rooms[this.currentRoomIndex].type === 'Auditorium' || this.rooms[this.currentRoomIndex].type === 'Classroom'
       || this.rooms[this.currentRoomIndex].type === 'Meetingroom') {
